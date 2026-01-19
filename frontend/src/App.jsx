dcb7839839ce33847d6e2629b93095d53460e799
B-6219 <brayokamau6219@gmail.com>
2026-01-19 03:32:42 -0500

    i have finsihed Header.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Project from './pages/Project'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className='bg-dark-surface'>
+
+      <Header />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/project' element={<Project />} />
+        {/* <Route path='/' element={ } /> */}
+      </Routes>
 
     </div>
   )
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Header = () => {
+    const navigate = useNavigate()
+
     return (
-        <div>Header</div>
+        <header className="bg-dark-surface">
+            <div className="max-w-7xl mx-auto px-6">
+                <div className="flex items-center justify-between h-16 text-sm">
+
+                    {/* Logo */}
+                    <div className="cursor-pointer" onClick={() => navigate('/')}>
+                        <h1 className="text-amber-400 text-3xl">Brian <span className="text-green-500">Kamau</span></h1>
+                    </div>
+
+                    {/* Nav links */}
+                    <ul className="hidden md:flex gap-6 font-medium">
+                        <NavLink className="text-accent-amber hover:text-neon" to="/">HOME</NavLink>
+                        <NavLink className="text-accent-amber hover:text-neon" to="/about">ABOUT ME</NavLink>
+                        <NavLink className="text-accent-amber hover:text-neon" to="/project">PROJECTS</NavLink>
+                        <NavLink className="text-accent-amber hover:text-neon" to="/contact">CONTACTS</NavLink>
+                    </ul>
+
+                    {/* CTA */}
+                    <button className="text-black bg-accent-amber hover:bg-neon px-5 py-2 border rounded-lg"> Hire Me</button>
+
+                </div>
+            </div>
+        </header>
     )
 }
 
